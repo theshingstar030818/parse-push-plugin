7da743b114a311c9e366926450fe22d541f55a7b
taivo <tai.a.vo@gmail.com>
2016-06-16 17:46:46 -0700

    android installation will now warn user if android:name is already set

--- scripts/beforeAndroidInstall.js
+++ scripts/beforeAndroidInstall.js
@@ -20,6 +20,8 @@ module.exports = function(context) {
    if(applicationNode.get('android:name') === undefined){
       applicationNode.set('android:name', DefaultApplicationName);
       androidManifest.save();
+   } else{
+      console.warn("It looks like your application's android:name has already been set. If you have not set it up for Parse.Push, please refer to the Advanced Configuration section of this plugin's README.");
    }
 
    return true;
