7e489a716174aee21aedb0a6cff004df1ddf560c
Minh Ha <mihado@minh.im>
2017-01-06 22:29:51 +1300

    log ServerUrl & requirement, clientKey, that Installation is saved in background

--- src/android/ParsePushApplication.java
+++ src/android/ParsePushApplication.java
@@ -63,17 +63,21 @@ public class ParsePushApplication extends Application {
             //
             //initialize for use with legacy parse.com
             Parse.initialize(this, config.getAppId(), config.getClientKey());
-         } else{
+         } else {
+            Log.d(LOGTAG, "ServerUrl " + config.getServerUrl());
+            Log.d(LOGTAG, "NOTE: The trailing slash is important, e.g., https://mydomain.com:1337/parse/");
+            Log.d(LOGTAG, "NOTE: Set the clientKey if your server requires it, otherwise it can be null");
             //
             // initialize for use with opensource parse-server
             Parse.initialize(new Parse.Configuration.Builder(this)
                .applicationId(config.getAppId())
-               .server(config.getServerUrl()) // The trailing slash is important, e.g., https://mydomain.com:1337/parse/
-               .clientKey(config.getClientKey()) // Can be null 
+               .server(config.getServerUrl())
+               .clientKey(config.getClientKey())
                .build()
             );
          }
 
+         Log.d(LOGTAG, "Saving Installation in background");
          //
          // save installation. Parse.Push will need this to push to the correct device
          ParseInstallation.getCurrentInstallation().saveInBackground(new SaveCallback() {
