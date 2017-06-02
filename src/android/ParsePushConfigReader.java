f9d2d9e2ec764a9924e930880386187adc9df2ca
Schwobaland <Schwobaland@users.noreply.github.com>
2017-06-02 14:37:34 +0200

    sdk update and format code

--- src/android/ParsePushConfigReader.java
+++ src/android/ParsePushConfigReader.java
@@ -17,103 +17,102 @@ import github.taivo.parsepushplugin.ParsePushConfigException;
 import android.util.Log;
 
 public final class ParsePushConfigReader {
-   //
-   // Extract relevant Parse.Push settings from config.xml
-   //
-   public static final String LOGTAG = "ParsePushConfigReader";
+  //
+  // Extract relevant Parse.Push settings from config.xml
+  //
+  public static final String LOGTAG = "ParsePushConfigReader";
 
-   private static final String preferenceTag = "preference"; //<preference name="ParseAppId" value="foo" />
+  private static final String preferenceTag = "preference"; //<preference name="ParseAppId" value="foo" />
 
-   //
-   // appId and serverUrl are required. They get special treatment, including their own get functions
-   //
-   private static final String parseAppIdKey = "ParseAppId";
-   private static final String parseServerUrlKey = "ParseServerUrl";
+  //
+  // appId and serverUrl are required. They get special treatment, including their own get functions
+  //
+  private static final String parseAppIdKey = "ParseAppId";
+  private static final String parseServerUrlKey = "ParseServerUrl";
 
-   // ParseClientKey is not required by parse server, but can be required by some environments
-   private static final String parseClientKeyKey = "ParseClientKey";
+  // ParseClientKey is not required by parse server, but can be required by some environments
+  private static final String parseClientKeyKey = "ParseClientKey";
 
-   private List<String> supportedKeys = new ArrayList(Arrays.asList(parseAppIdKey, parseServerUrlKey));
-   private Map<String, String> configs;
+  private List<String> supportedKeys = new ArrayList(Arrays.asList(parseAppIdKey, parseServerUrlKey));
+  private Map<String, String> configs;
 
-   private static int getConfigXmlResourceId(Context context){
-      return context.getResources().getIdentifier("config", "xml", context.getPackageName());
-   }
+  private static int getConfigXmlResourceId(Context context) {
+    return context.getResources().getIdentifier("config", "xml", context.getPackageName());
+  }
 
+  public ParsePushConfigReader(Context context, Integer configXmlResourceId, String[] moreSupportedKeys)
+      throws ParsePushConfigException {
+    int configXmlResId = (configXmlResourceId == null ? getConfigXmlResourceId(context) : configXmlResourceId);
 
-   public ParsePushConfigReader(Context context, Integer configXmlResourceId, String[] moreSupportedKeys) throws ParsePushConfigException{
-      int configXmlResId = (configXmlResourceId == null ? getConfigXmlResourceId(context) : configXmlResourceId);
+    if (moreSupportedKeys != null) {
+      supportedKeys.addAll(Arrays.asList(moreSupportedKeys));
+    }
 
-      if(moreSupportedKeys != null){
-         supportedKeys.addAll(Arrays.asList(moreSupportedKeys));
+    configs = loadConfigsFromXml(context.getResources(), configXmlResId);
+    if (!hasRequiredParams()) {
+      throw new ParsePushConfigException(
+          parseAppIdKey + " or " + parseServerUrlKey + " is missing! Please set them in config.xml");
+    }
+  }
+
+  public String getAppId() {
+    return configs.get(parseAppIdKey);
+  }
+
+  public String getServerUrl() {
+    return configs.get(parseServerUrlKey);
+  }
+
+  public String getClientKey() {
+    return configs.get(parseClientKeyKey);
+  }
+
+  public String get(String key) {
+    return configs.get(key);
+  }
+
+  public Boolean hasRequiredParams() {
+    return !(getAppId() == null || getServerUrl() == null || getAppId().isEmpty() || getServerUrl().isEmpty());
+  }
+
+  private String matchSupportedKeyName(String testKey) {
+    //
+    // If key matches, return the version with correct casing.
+    // If not, return null.
+    // O(n) here is okay because this is a short list of just a few items
+    for (String realKey : supportedKeys) {
+      if (realKey.equalsIgnoreCase(testKey)) {
+        return realKey;
       }
+    }
+    return null;
+  }
 
-      configs = loadConfigsFromXml(context.getResources(), configXmlResId);
-      if(!hasRequiredParams()){
-         throw new ParsePushConfigException(parseAppIdKey + " or " + parseServerUrlKey + " is missing! Please set them in config.xml");
+  private Map loadConfigsFromXml(Resources res, int configXmlResourceId) {
+    XmlResourceParser xrp = res.getXml(configXmlResourceId);
+
+    Map configs = new HashMap();
+
+    //
+    // walk the config.xml tree and save all <preference> tags related to Parse.Push
+    //
+    try {
+      xrp.next();
+      while (xrp.getEventType() != XmlResourceParser.END_DOCUMENT) {
+        if (preferenceTag.equals(xrp.getName())) {
+          String key = matchSupportedKeyName(xrp.getAttributeValue(null, "name"));
+          if (key != null) {
+            configs.put(key, xrp.getAttributeValue(null, "value"));
+          }
+        }
+        xrp.next();
       }
-   }
+    } catch (XmlPullParserException ex) {
+      Log.e(LOGTAG, ex.toString());
+    } catch (IOException ex) {
+      Log.e(LOGTAG, ex.toString());
+    }
 
-   public String getAppId(){
-      return configs.get(parseAppIdKey);
-   }
-
-   public String getServerUrl(){
-      return configs.get(parseServerUrlKey);
-   }
-
-   public String getClientKey(){
-       return configs.get(parseClientKeyKey);
-   }
-
-   public String get(String key){
-      return configs.get(key);
-   }
-
-   public Boolean hasRequiredParams(){
-      return !(getAppId() == null   || getServerUrl() == null ||
-               getAppId().isEmpty() || getServerUrl().isEmpty()
-               );
-   }
-
-   private String matchSupportedKeyName(String testKey){
-      //
-      // If key matches, return the version with correct casing.
-      // If not, return null.
-      // O(n) here is okay because this is a short list of just a few items
-      for(String realKey : supportedKeys){
-         if( realKey.equalsIgnoreCase(testKey)){
-            return realKey;
-         }
-      }
-      return null;
-   }
-
-   private Map loadConfigsFromXml(Resources res, int configXmlResourceId){
-      XmlResourceParser xrp = res.getXml(configXmlResourceId);
-
-      Map configs = new HashMap();
-
-      //
-      // walk the config.xml tree and save all <preference> tags related to Parse.Push
-      //
-      try{
-         xrp.next();
-         while(xrp.getEventType() != XmlResourceParser.END_DOCUMENT){
-            if(preferenceTag.equals(xrp.getName())){
-               String key = matchSupportedKeyName(xrp.getAttributeValue(null, "name"));
-               if(key != null){
-                  configs.put(key, xrp.getAttributeValue(null, "value"));
-               }
-            }
-            xrp.next();
-         }
-      } catch(XmlPullParserException ex){
-         Log.e(LOGTAG, ex.toString());
-      }  catch(IOException ex){
-         Log.e(LOGTAG, ex.toString());
-      }
-
-      return configs;
-   }
+    return configs;
+  }
 }
